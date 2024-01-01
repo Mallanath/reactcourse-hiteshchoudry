2377cad534be24ebdf5fea6eb0bf846e5d131529
Mallanath <mallanathmise443@gmail.com>
2024-01-01 19:44:06 +0530

    second commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,15 @@
+import Youtube from "./Youtube"
 
 
 function App() {
-  
+  const username = "mallanath"
 
   return (
-   <h1>vite react app</h1>
+   <>
+   <h1>vite react app {2+2}</h1>
+   <h1>vite react app {username}</h1>
+   <Youtube/>
+   </>
   )
 }
 
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: '_blank'
+        
+    },
+    childen: 'Click me to visit google'
+}
+
+function MyApp(){
+    return (
+        <div>
+            <h1>Custom react hook</h1>
+        </div>
+    )
+}
+
+const AnotherElement = (
+    <a href="https://google.com" target='blank'>Visit Google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: "_blank"},
+    'click to visit google'
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+   <App/>
+  
 )
