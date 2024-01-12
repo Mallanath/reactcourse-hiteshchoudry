d63e05f0b51fd0743aa014cbaa11959c386a5475
Mallanath <mallanathmise443@gmail.com>
2024-01-12 21:06:00 +0530

    todo app with context api

--- 08minicontext/src/components/Profile.jsx
+++ 08minicontext/src/components/Profile.jsx
@@ -8,7 +8,7 @@ function Profile() {
   if(!user) return <h1>Not logged in</h1>
 
   return (
-    <div><h1>Profile :{user.username}</h1> 
+    <div><h1>Profile :{user.username} {" "} {user.password}</h1> 
     </div>
   )
 }
